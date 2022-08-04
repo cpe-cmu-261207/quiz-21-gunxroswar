888e36f42eb03c9b36144ad5da008d03d8a770b6
gunxroswar <gxz.gunforce@gmail.com>
2022-08-04 14:11:17 +0700

    Mamamamamamam

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 async function callApi() {
-  const resp = await axios.get("https://randomuser.me/api/");
+  const resp = await axios.get("https://randomuser.me/api/%22);
+  const x = resp.result[0];
+  console.log(x.gender);
 }
+
+const div = document.getElementById("div-user-card");
+const img = document.getElementById("img-profile");
+const names = document.getElementById("p-name");
+const address = document.getElementById("p-address");
+const email = document.getElementById("p-email");
+const button = document.getElementById("btn-random");
+
+button.onclick = () => {
+  names.innerText =  ${x.name.title} ${x.name.first} ${x.name.last};
+  email.innerText = ${x.email};
+  address.innerText = ${x.location.city} ${x.location.state} ${x.location.country} ${x.location.postcode};
+};
